cef6b87cb481b4e06064ca7d1824ba546ebb153b
lanzv <vojtech.lanz@gmail.com>
2021-04-17 22:02:08 +0200

    Add function for eval isophonics test dataset

--- ACR_Training/Evaluator.py
+++ ACR_Training/Evaluator.py
@@ -18,6 +18,51 @@ class Evaluator():
     """
 
     @staticmethod
+    def eval_isophonics_testset(annotations_directory, predictions, sample_rate=22050, hop_length=512):
+        """
+        The function will evaluate weighted accuracy score of lab files and predictions of billboard test dataset, using mir eval library.
+
+        Parameters
+        ----------
+        annotations_directory : str
+            path of the annotations directory containing .lab files
+        predictions : int list list
+            path of the .LAB file that contains predicted chords of the song
+        sample_rate : int
+            audio sample rate
+        hop_length : int
+            number of samples between successive spectrogram columns
+        Returns
+        -------
+        score : float
+            mir_eval score, weighted accuracy
+        """
+        durations = np.empty(1)
+        comparisons = np.empty(1)
+
+        # Get list of lab files
+        lab_files = sorted(glob(os.path.join(annotations_directory+'/CHORDS', '*.lab')))
+
+        # Collect durations and comparisons
+        for lab, chord_sequence in zip(lab_files, predictions):
+            n_sequences,n_frames,n_features = chord_sequence.shape
+            chord_sequence = np.array(chord_sequence.reshape((n_sequences*n_frames,n_features))).argmax(axis=1)
+            ref_intervals, ref_labels = mir_eval.io.load_labeled_intervals(lab)
+            est_intervals, est_labels = Evaluator._get_label_intervals(chord_sequence=chord_sequence, sample_rate=sample_rate, hop_length=hop_length)
+            est_intervals, est_labels = mir_eval.util.adjust_intervals(est_intervals, est_labels, ref_intervals.min(), ref_intervals.max(), mir_eval.chord.NO_CHORD, mir_eval.chord.NO_CHORD)
+            (intervals, ref_labels, est_labels) = mir_eval.util.merge_labeled_intervals(ref_intervals, ref_labels, est_intervals, est_labels)
+            durations = np.insert(durations, -1, mir_eval.util.intervals_to_durations(intervals))
+            comparisons = np.insert(comparisons, -1 , mir_eval.chord.triads(ref_labels, est_labels))
+
+        # Get Score
+        durations = durations[:-1]
+        comparisons = comparisons[:-1]
+        score = mir_eval.chord.weighted_accuracy(comparisons, durations)
+
+        return score
+
+
+    @staticmethod
     def eval_billboard_testset(annotations_directory, predictions, sample_rate=22050, hop_length=512):
         """
         The function will evaluate weighted accuracy score of lab files and predictions of billboard test dataset, using mir eval library.
